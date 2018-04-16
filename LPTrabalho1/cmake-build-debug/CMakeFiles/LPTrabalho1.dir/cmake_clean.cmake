6751703b784f4d492437fdcd7ad6efda11059c3f
Edson Onildo <edsononildo@id.uff.br>
2018-04-16 20:09:10 -0300

    Initial commit

---
+++ LPTrabalho1/cmake-build-debug/CMakeFiles/LPTrabalho1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LPTrabalho1.dir/main.c.o"
+  "LPTrabalho1.pdb"
+  "LPTrabalho1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LPTrabalho1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
